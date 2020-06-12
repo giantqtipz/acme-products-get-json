c1f6a0c2fd433255b1ab4736587cd59c72e3f4ac
Bima Saridjo <palessie27@gmail.com>
2020-06-12 14:52:17 -0400

    push workshop

---
+++ db.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.listen(3000, () => {
+    console.log('port: 3000');
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, `./index.html`)); 
+})
+
+app.get('/api/products', (req, res) => {
+    res.sendFile(path.join(__dirname, `./products.json`)); 
+})
+
+const readJSON = (file)=> {
+  return new Promise((resolve, reject)=> {
+    fs.readFile(file, (err, data)=> {
+      if(data){
+        try {
+          resolve(JSON.parse(data.toString()));
+        }
+        catch(ex){
+          reject(ex);
+        }
+      }
+      else {
+        reject(err);
+      }
+    });
+  });
+};
+
+module.exports = {
+  readJSON
+};
